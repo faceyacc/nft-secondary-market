53b14487f16ef0796e608c6617e7f3695e2a918c
Ty <justfacey@gmail.com>
2021-10-26 21:37:11 -0400

    Added create item and asset pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,45 @@
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 import '../styles/base.css'
 
+
+
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">NFT Secondary Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-blue-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-items">
+            <a className="mr-6 text-blue-500">
+              Sell Digital Asset
+            </a>
+          </Link>
+
+          <Link href="/my-assets">
+            <a className="mr-6 text-blue-500">
+              My Digital Assets
+            </a>
+          </Link>
+
+          <Link href="/creator-dashboard">
+          <a className="mr-6 text-blue-500">
+            Creator Dashboard
+          </a>
+          </Link>
+        </div>
+      </nav>
+      <Component { ...pageProps } />
+    </div>
+  )
 }
+
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,116 @@
-function Homepage() {
-    return (
-      <h1 className="text-center my-24 font-black tracking-tight text-6xl">Our homepage</h1>
-    )
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios, { AxiosResponse } from 'axios'
+import Web3Model from 'web3modal'
+import {
+  nftaddress, nftmarketaddress
+} from '../config'
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+
+
+function Home() {
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+  useEffect(() => {
+    loadNFTs()
+  }, [])
+
+  interface metadataType {
+    image: string,
+    name: string
+    desription: string,
   }
+
+  type nftType = {
+    price: string
+    tokenId: number
+    seller: string
+    owner: string
+    image: unknown
+    name: unknown
+    desription: unknown
+  }
+
+  async function loadNFTs() {
+    // Query for unsold market items.
+    const provider =  new ethers.providers.JsonRpcProvider()
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+    
+    // Fetch all market items that are for sale.
+    const data:nftType[] = await marketContract.fetchMarketItems()  
+
+
+    // Map over items retruned from smart contract.
+    const items = await Promise.all(data.map(async (i:nftType) => {
+      const tokenUri: string = await tokenContract.tokenURI(i.tokenId)
+      
+      // Axios call returns meta data of NFT : https://gateway.pinata.cloud/ipfs/QmSvBcb4tjdFpajGJhbFAWeK3JAxCdNQLQtr6ZdiSi42V2
+      const meta: AxiosResponse<metadataType> = await axios.get(tokenUri) 
+      const price: string = ethers.utils.formatUnits(i.price.toString(), 'ether')
+
+      // return NFTs
+      let item: nftType = {
+        price, 
+        tokenId: i.tokenId,
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name, 
+        desription: meta.data.desription,
+      }
+      return item
+    }))
+    setNfts(items)
+    setLoadingState('loaded')
+  }
+
+  async function buyNFTs(nft: nftType) {
+    // Request signature from user for transaction.
+    const web3Modal = new Web3Model()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+
+    // Prompts user to pay to complete transaction.
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
+    const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, {
+      value: price
+    })
+    await transaction.wait()
+    loadNFTs()
+  }
+  if(loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>) 
+    return (
+      <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: '1600px' }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {
+            nfts.map((nft, i) => (
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} />
+                <div className="p-4">
+                  <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
+                  <div style={{ height: '70px', overflow: 'hidden' }}>
+                    <p className="text-gray-400">{nft.description}</p>
+                  </div>
+                </div>
+                <div className="p-4 bg-black">
+                  <p className="text-2xl mb-4 font-bold text-white">{nft.price} ETH</p>
+                  <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNFTs(nft)}>Buy</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+    )
+}
+
+
   
-  export default Homepage
+export default Home
